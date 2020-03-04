110b5ca8357b869ba575cef48efb08e32e5ed605
eternal golden bread <codylewisbrock@gmail.com>
2020-03-04 07:55:58 -0800

    fixes handleMonkeyClick so that the correct object is foundupon clicks

--- src/components/MonkeyContainer.js
+++ src/components/MonkeyContainer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 import NavBar from "./NavBar/NavBar"
 import Jumbotron from "./Jumbotron/Jumbotron";
@@ -18,24 +18,42 @@ const MonkeyContainer = () => {
   const monkeyArrLength = monkeys.length;
 
   const randomizeMonkeyCards = () => {
+    //
+    console.log('monkeys', monkeys)
     let newMonkeys = monkeys;
-    // console.log('newMonkeys', newMonkeys)
+    console.log("newMonkeys", newMonkeys)
     let returnMonkeys = [];
     while (returnMonkeys.length < monkeyArrLength) {
       let monkeyIndex = randomIntFromInterval(0, newMonkeys.length-1);
       let monkeyPush = newMonkeys[monkeyIndex];
       returnMonkeys.push(monkeyPush);
       newMonkeys.splice(monkeyIndex, 1);
-      // console.log('returnMonkeys', returnMonkeys)
-      // console.log('newMonkeys', newMonkeys)
-      // console.log('monkeyIndex', monkeyIndex)
     }
+    console.log("returnMonkeys", returnMonkeys);
     setMonkeys(returnMonkeys);
+    console.log('after setMonkeys monkeys', monkeys)
   }
 
-  const handleMonkeyClick = id => {
-    console.log("Monkey clicked: ", id);
-    randomizeMonkeyCards()
+  // Method to handle when user clicks on Monkey images
+  const handleMonkeyClick = clickedId => {
+    // Finds the correct object in our monkey array
+    const clickedMonkey = monkeys.filter(monkey => monkey.id===clickedId);
+    console.log("correctMonkey[0]", clickedMonkey[0]);
+
+    // If monkey has already been clicked...
+    if (clickedMonkey[0].alreadyClicked === true) {
+      // Losing condition
+      alert("Already Clicked!");
+    } else {
+      // Winning condition
+      console.log("hooray");
+      // Sets property so we know this monkey has been clicked
+      clickedMonkey[0].alreadyClicked = true;
+      // Updates NavBar
+      NavBar(1, 69)
+      // Triggers method to randomize then rerender cards
+      randomizeMonkeyCards()
+    }
   }
 
   return(
@@ -43,6 +61,7 @@ const MonkeyContainer = () => {
       <NavBar 
         score={2}
         topScore={5}
+        // handleScore={() => handleScore()}
       />
       <Jumbotron />
       
